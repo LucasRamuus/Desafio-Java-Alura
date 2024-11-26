2ded109cac4608972e9c0ccba757ae3cbc909181
LucasRamuus <lucasgabrielpr12@gmail.com>
2024-11-26 18:08:42 -0300

    Novas atividades

---
+++ OrientadoObjeto/ConceitosFundamentais/Carro/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Carro meuCarro = new Carro();
+        meuCarro.modelo = "Gol";
+        meuCarro.ano = 2023;
+        meuCarro.cor = "Preto";
+
+        meuCarro.exibeFichaTecnica();
+
+        System.out.println("Idade do carro: " + meuCarro.calculaIdade() + " anos");
+
+    }
+}
--- Soma_Subtracao/src/Soma.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class Soma {
-
-    public static void main(String[] args){
-
-        System.out.println(10+5);
-    }
-}
